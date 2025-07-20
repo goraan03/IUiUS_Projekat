6660bf52f16b8dc926d5648cdd71ba7f02c53561
goraan03 <gorangrcic72@gmail.com>
2025-07-20 22:56:44 +0200

    Views
    Dodat prozor i logika za Login

---
+++ Views/LoginWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using IUiUS_Projekat.Models;
+using IUiUS_Projekat.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace IUiUS_Projekat.Views
+{
+    public partial class LoginWindow : Window
+    {
+        private readonly List<User> users;
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+            users = UserService.LoadUsers();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameBox.Text;
+            string password = PasswordBox.Password;
+
+            var user = users.FirstOrDefault(u => u.Username == username && u.Password == password);
+
+            if (user == null)
+            {
+                MessageBox.Show("Pogrešno korisničko ime ili lozinka.", "Greška", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var mainWindow = new MainWindow(user);
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
