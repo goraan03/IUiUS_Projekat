9d300f051f8be1f6733bb4811c0b8783c1af249e
goraan03 <gorangrcic72@gmail.com>
2025-07-20 23:00:15 +0200

    MainWindow
    Izgled MainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,66 @@
-﻿using System.Text;
+﻿using IUiUS_Projekat.Models;
+using IUiUS_Projekat.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace IUiUS_Projekat
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private ObservableCollection<KursViewModel> kurseviVM;
+
+        public MainWindow(User user)
         {
             InitializeComponent();
+            LoadData();
+            this.Title = $"Dobrodošao, {user.Username} ({user.Role})";
         }
+
+        private void LoadData()
+        {
+            var kursevi = KursService.LoadKursevi();
+            kurseviVM = new ObservableCollection<KursViewModel>(
+                kursevi.Select(k => new KursViewModel
+                {
+                    Naziv = k.Naziv,
+                    Cena = k.Cena,
+                    SlikaPath = k.SlikaPath,
+                    OpisRtfPath = k.OpisRtfPath,
+                    DatumDodavanja = k.DatumDodavanja,
+                    IsSelected = false
+                }));
+            KurseviGrid.ItemsSource = kurseviVM;
+        }
+
+        private void SelectAll_Checked(object sender, RoutedEventArgs e)
+        {
+            foreach (var kurs in kurseviVM)
+                kurs.IsSelected = true;
+            KurseviGrid.Items.Refresh();
+        }
+
+        private void SelectAll_Unchecked(object sender, RoutedEventArgs e)
+        {
+            foreach (var kurs in kurseviVM)
+                kurs.IsSelected = false;
+            KurseviGrid.Items.Refresh();
+        }
+
+        private void Naziv_Click(object sender, RoutedEventArgs e)
+        {
+            var link = (Hyperlink)sender;
+            var kurs = (KursViewModel)((TextBlock)link.Parent).DataContext;
+            MessageBox.Show($"Kliknuto na: {kurs.Naziv}");
+        }
+    }
+
+    public class KursViewModel : Kurs
+    {
+        public bool IsSelected { get; set; }
     }
 }
