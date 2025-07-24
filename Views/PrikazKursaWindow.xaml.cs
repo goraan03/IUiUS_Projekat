ac152f0aaa1505767ec18d264abc3559f5fd41a4
goraan03 <gorangrcic72@gmail.com>
2025-07-24 02:35:33 +0200

    Prikaz kursa za visitor

---
+++ Views/PrikazKursaWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using IUiUS_Projekat.Models;
+using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace IUiUS_Projekat.Views
+{
+    public partial class PrikazKursaWindow : Window
+    {
+        public PrikazKursaWindow(Kurs kurs)
+        {
+            InitializeComponent();
+            DataContext = kurs;
+
+            if (File.Exists(kurs.OpisRtfPath))
+            {
+                using (FileStream fs = new FileStream(kurs.OpisRtfPath, FileMode.Open))
+                {
+                    TextRange range = new TextRange(OpisBox.Document.ContentStart, OpisBox.Document.ContentEnd);
+                    range.Load(fs, System.Windows.DataFormats.Rtf);
+                }
+            }
+        }
+
+        private void Zatvori_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
